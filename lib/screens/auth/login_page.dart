9d11449d8d2d9ccafe98ca6ce3b1eff4462021fc
Usersklkjk <gimhansusara0@gmail.com>
2025-06-05 20:07:10 +0530

    0.04
    Made the Logo Component for Login page

--- lib/screens/auth/login_page.dart
+++ lib/screens/auth/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
+import 'package:urban_toast/screens/auth/components/login/login_form.dart';
+import 'package:urban_toast/screens/auth/components/login/login_img.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -9,126 +10,43 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: Container(
+  body: OrientationBuilder(
+    builder: (context, orientation) {
+      if (orientation == Orientation.portrait) {
+        return _portraitBuilder(context);
+      } else {
+        return _landscapeBuilder();
+      }
+    },
+  ),
+);
+  }
+}
+
+Widget _portraitBuilder(BuildContext context) {
+  return SingleChildScrollView(
+      child: Container(
         width: double.infinity,
-        decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-        child: SingleChildScrollView(
-          physics: ClampingScrollPhysics(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height * 0.4,
-                child: Center(
-                  child: Image.asset(
-                    Theme.of(context).brightness == Brightness.dark
-                        ? "assets/images/logo-black.png"
-                        : "assets/images/logo-white.png",
-                    width: 150,
-                    height: 150,
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height * 0.6,
-                decoration: BoxDecoration(
-                    borderRadius:
-                        BorderRadius.only(topLeft: Radius.circular(100)),
-                    color: Theme.of(context).scaffoldBackgroundColor),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
-                      child: Text(
-                        'Welcome Back!',
-                        style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(0, 30, 0, 20),
-                      width: 350,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Email',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          TextField(
-                            controller: emailController,
-                            decoration:
-                                InputDecoration(hintText: 'Enter your email'),
-                          ),
-                          SizedBox(height: 16),
-                          Text('Password',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold)),
-                          TextField(
-                            controller: passwordController,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                hintText: 'Enter your password'),
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          SizedBox(
-                            width: double.infinity,
-                            height: 50,
-                            child: ElevatedButton(
-                                onPressed: () {},
-                                child: Text(
-                                  'Login',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                  ),
-                                )),
-                          )
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
-                      child: RichText(
-                          text: TextSpan(
-                              text: "Don't have an account? ",
-                                style: TextStyle(
-                                  color: Theme.of(context).brightness == Brightness.dark
-                                      ? Colors.white
-                                      : Theme.of(context).primaryColorDark,
-                                ), children: [
-                            TextSpan(
-                                text: 'Sign Up',
-                                style: TextStyle(
-                                    color: Theme.of(context).primaryColor),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () {
-                                    print('Signup Clicked');
-                                  })
-                          ])),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.4,
+              child: LoginImg(),
+            ),
+            Expanded(child: LoginForm())
+          ],
         ),
       ),
-    );
-  }
+  );
+}
+
+Widget _landscapeBuilder() {
+  return Container();
 }
